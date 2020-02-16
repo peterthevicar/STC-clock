25c503aab96ccbdb6f4680f045653a886dc39e31
peterthevicar <peter@lymingtonchurch.org>
2020-02-16 20:03:09 +0000

    Simplified and used same pinout as the driver board

--- motor-control.py
+++ motor-control.py
@@ -8,64 +8,58 @@ import RPi.GPIO as GPIO
 # instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
  
-# Define GPIO signals to use
-# In order [Y,Pi,Bu,O]
-StepPins = [15,14,4,18]
+# Define GPIO pins to use to drive the motor
+# In order    Bu, Pi, Y, O (same as motor driver board)
+motor_pins = (4, 14, 15, 18)
  
 # Set all pins as output
-# According to raspberry-gpio-python documentation (https://sourceforge.net/p/raspberry-gpio-python/wiki/Outputs/) 
-# you can output to several channels at the same time by passing 2 list parameters instead of 2 integers.
-for pin in StepPins:
-  print("Setup pins")
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False) # the ULN2003 driver inverts the voltage, i.e. True is 0v
+# According to raspberry-gpio-python documentation at
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/Outputs/
+# you can output to several channels at the same time
+# by passing 2 list parameters instead of 2 integers.
+GPIO.setup(motor_pins, GPIO.OUT)
 
-# Define advanced sequence
-# as shown in manufacturer's datasheet (these are reversed, i.e. 1 = 0v) see above
-# Sequence starts at step 2 (step 1 is Orange only at 0v)
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
+# Sequence of outputs to the motor_pins to turn the motor clockwise,
+# as shown in manufacturer's datasheet.
+# These are reversed, i.e. 1 = 0v, because the ULN2003 inverts the voltage
+motor_seq = (
+    (0,0,0,1),
+    (0,0,1,1),
+    (0,0,1,0),
+    (0,1,1,0),
+    (0,1,0,0),
+    (1,1,0,0),
+    (1,0,0,0),
+    (1,0,0,1)
+    )
  
-StepCount = len(Seq)
-StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
+step_count = len(motor_seq)
+# Set step_inc to +/-1 or +/-2. 1 gives half speed, high torque.
+# Positive increment turns the shaft clockwise, negative anti-clockwise
+# The duration of a 360° rotation is wait_sec*(step_count/step_inc)*128
+# so for an increment of 1 you get wait_sec*1024 per rotation
+step_inc = int(input("increment (-2,-1,1,2): "))
  
-# Read wait time from command line
-if len(sys.argv) > 1:
-  WaitTime = int(sys.argv[1])/float(1000)
-else:
-  WaitTime = 10/float(1000)
+# Read wait time from terminal
+wait_sec = float(input("Wait time (s): "))
  
 # Initialise variables
-StepCounter = 0
+step_n = 0
  
 # Start main loop
-while True:
- 
-  print(StepCounter, Seq[StepCounter])
- 
-  for pin in range(0, 4):
-    xpin = StepPins[pin]#
-    if Seq[StepCounter][pin]!=0:
-      print(" Enable GPIO", pin)
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
- 
-  StepCounter += StepDir
- 
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter >= StepCount):
-    StepCounter = 0
-  if (StepCounter < 0):
-    StepCounter = StepCount+StepDir
- 
-  # Wait before moving on
-  time.sleep(WaitTime)
+try:
+    while True:
+      GPIO.output(motor_pins, motor_seq[step_n]) 
+      step_n += step_inc
+     
+      # loop at end of sequence
+      if (step_n >= step_count):
+        step_n = 0
+      elif (step_n < 0):
+        step_n = step_count + step_inc
+     
+      # Wait between steps
+      time.sleep(wait_sec)
+
+finally:
+    GPIO.output(motor_pins, False)
