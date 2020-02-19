145bbecd897861df17cfd79d574870e9d659831d
peterthevicar <peter@lymingtonchurch.org>
2020-02-19 00:36:14 +0000

    Control motor using interrupt

--- interrupt-test.py
+++ interrupt-test.py
@@ -4,16 +4,17 @@
 import datetime
 import RPi.GPIO as GPIO  
 GPIO.setmode(GPIO.BCM)  
+
+interrupt_pin = 2
+# GPIO set up as input. It is pulled up to stop false signals  
+GPIO.setup(interrupt_pin, GPIO.IN) # pin 2 has pull up already, pull_up_down=GPIO.PUD_UP)  
   
-# GPIO 23 set up as input. It is pulled up to stop false signals  
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-  
-print ("Waiting for falling edge on port 23")  
+print ("Waiting for falling edge on GPIO", interrupt_pin)  
 # now the program will do nothing until the signal on port 23   
 # starts to fall towards zero. This is why we used the pullup  
 # to keep the signal high and prevent a false interrupt  
   
 while True:
-    GPIO.wait_for_edge(23, GPIO.FALLING)  
+    GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)  
     print (datetime.datetime.now())
 GPIO.cleanup()           # clean up GPIO on normal exit  
