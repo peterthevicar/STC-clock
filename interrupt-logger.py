7ccd187cc3be0c32a4ef071d268da70b581a3b98
peterthevicar <peterbcs@gmail.com>
2020-02-29 22:12:41 +0000

    Add in settings.txt to record current setting of fine adjustment

--- interrupt-logger.py
+++ interrupt-logger.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+# For testing in diffierent situations
+USING_GPIO=False
+datadir="/home/pi/Desktop/STC-clock/Data/"
+logfile=datadir+"interrupts.log"
+setfile=datadir+"setting.txt"
+
 # Set up rotating log file
 import logging
 import time
@@ -8,24 +14,29 @@ from logging.handlers import TimedRotatingFileHandler
 logger = logging.getLogger("Rotating Log")
 logger.setLevel(logging.INFO)
 # Set up log to rotate daily
-handler = TimedRotatingFileHandler("/home/pi/Desktop/STC-clock/Data/interrupts.log",
+handler = TimedRotatingFileHandler(logfile,
                                    when="d",
                                    interval=1,
                                    backupCount=0)
 logger.addHandler(handler)
 
 # Set up interrupt handler
-import datetime
-import RPi.GPIO as GPIO  
-GPIO.setmode(GPIO.BCM)  
+if USING_GPIO:
+    import RPi.GPIO as GPIO  
+    GPIO.setmode(GPIO.BCM)  
 
-interrupt_pin = 2
-# GPIO set up as input. It is pulled up to stop false signals
-GPIO.setup(interrupt_pin, GPIO.IN) # pin 2 has pull up already, pull_up_down=GPIO.PUD_UP)  
+    interrupt_pin = 2
+    # GPIO set up as input. It is pulled up to stop false signals
+    GPIO.setup(interrupt_pin, GPIO.IN) # pin 2 has pull up already, pull_up_down=GPIO.PUD_UP)  
+
+import datetime
     
 while True:
-    GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)
-    logger.info(datetime.datetime.now())
+    if USING_GPIO: GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)
+    interrupt_time = datetime.datetime.now()
+    with open(setfile) as f:
+        current_setting = f.readline().rstrip()
+    logger.info(current_setting + " " + str(interrupt_time))
     time.sleep(1) # anything less than 1 second is a bounce
     
 GPIO.cleanp()           # clean up GPIO on normal exit  
