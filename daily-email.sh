6e9e82ae889be7f0ec78c7f59cde073db4d42c5d
peterthevicar <peter@lymingtonchurch.org>
2020-05-21 22:57:30 +0100

    forget outliers now we're using medians

--- daily-email.sh
+++ daily-email.sh
@@ -5,7 +5,7 @@ cd /home/pi/Desktop/STC-clock
 	cat msmtp-headers.txt
 	cat Data/daily-correction.log | awk -v RS='---\n' 'END { print $0 }'
 	echo ""
-	python3 data-analysis.py -f -d10 <Data/interrupts.log
+	python3 data-analysis.py -f -d0 <Data/interrupts.log
 	echo ""
 	cat Data/interrupts.log
 } | msmtp "$(grep To msmtp-headers.txt | sed "s/To: //")"
