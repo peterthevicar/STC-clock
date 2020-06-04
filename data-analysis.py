b1e0f7ebf25fb1fd3bfb9bf9901d78e71b587d8c
peterthevicar <peterbcs@gmail.com>
2020-06-04 12:05:42 +0100

    Analyse the temperature data if present

--- data-analysis.py
+++ data-analysis.py
@@ -41,7 +41,8 @@ for inline in sys.stdin:
             print("First data point:", dt_nearest_min)
             report_first = False
         # ~ print("actual:", dt_actual, "nearest:", dt_nearest_min, "err:", err) # DEBUG
-        err_data.append([dt_secs, dt_nearest_min, err, fine_adjust])
+        cur_temp = float(s[3]) if len(s) >= 4 else -99
+        err_data.append([dt_secs, dt_nearest_min, err, fine_adjust, cur_temp])
         dt_nearest_min_prev = dt_nearest_min
 # ~ print(err_data) # DEBUG
 # Calculate average, omitting top and bottom extreme values as outliers
@@ -51,17 +52,21 @@ out_end = -out_start if out_start > 0 else None
 sum_fa = 0
 sum_err = 0; n_err = 0; max_err = -100; min_err = 100
 sum_dt = 0
+sum_temp = 0; n_temp = 0
 sorted_data=sorted(err_data, key=lambda err_data: err_data[2])[out_start:out_end]
 med_min, med_err = sorted_data[len(sorted_data)//2][1:3]
 print("ERROR", "{:.3f}".format(med_err), "(Median Error at",med_min,", positive means clock is slow)")
 # ~ print (sorted(sorted_data)) # DEBUG
 if args.full: # full analysis requested
-    for dt_secs, dt_nearest_min, err, fine_adjust in sorted_data:
+    for dt_secs, dt_nearest_min, err, fine_adjust, cur_temp in sorted_data:
         sum_fa += fine_adjust
         if err > max_err: max_err=err
         if err < min_err: min_err=err
         sum_err += err
         sum_dt += dt_secs
+        if cur_temp != -99:
+            sum_temp += cur_temp
+            n_temp += 1
         n_err += 1
         # ~ print(dt_secs, err)
     avg_err = sum_err / n_err
@@ -69,11 +74,12 @@ if args.full: # full analysis requested
     # Now we know the average we can calculate the least-square slope
     avg_fa = sum_fa / n_err
     avg_dt_secs = sum_dt / n_err
+    avg_temp = sum_temp / n_temp if n_temp > 0 else -99
     # Can't fit the curve with half hours included; the numbers are too jittery
     if not ACCEPT_HALF:
         # ~ print("avg_dt_secs:", avg_dt_secs)
         sum_xy = 0; sum_x_sq = 0
-        for dt_secs, dt_nearest_min, err, fine_adjust in sorted_data:
+        for dt_secs, dt_nearest_min, err, fine_adjust, cur_temp in sorted_data:
             sum_xy += (dt_secs - avg_dt_secs)*(err - avg_err)
             sum_x_sq += (dt_secs - avg_dt_secs)**2
         slope = sum_xy/sum_x_sq
@@ -83,6 +89,7 @@ if args.full: # full analysis requested
     else:
         print("TREND - (Not fitting line to data points because half hours are included)")
     print("FINEA","{:.2f}".format(avg_fa),"(Average fine adjust setting)")
+    print("AVGTP","{:.2f}".format(avg_temp),"(Average temperature in clock case)")
     print("  Maximum error:","{:.3f}".format(max_err),"= average error +","{:.3f}".format(max_err-avg_err))
     print("  Minimum error:","{:.3f}".format(min_err),"= average error -","{:.3f}".format(avg_err-min_err))
     print("  Spread of values (max-min):", "{:.3f}".format(max_err - min_err))
