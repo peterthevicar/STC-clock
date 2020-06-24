9f3f057e362e098685bb2933128050347bebcb9e
peterthevicar <peterbcs@gmail.com>
2020-06-24 13:58:32 +0100

    Change interrupt pin to 22 so it's not interfering with i2c Include code to read from the bme280 multi sensor Include all the sensor data after each interrupt (temperature, pressure, humitiy)

--- interrupt-logger.py
+++ interrupt-logger.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-# For testing in diffierent situations
+# For testing in different situations
 USING_GPIO=True
+USE_LOG=True
+
+# File paths
 datadir="/home/pi/Desktop/STC-clock/Data/"
 logfile=datadir+"interrupts.log"
 setfile=datadir+"setting.txt"
@@ -9,7 +12,21 @@ setfile=datadir+"setting.txt"
 # Each temperature sensor has a different address so if you change the sensor change this
 temp_sensor="/sys/bus/w1/devices/28-00181100004b/w1_slave"
 
+#
+# Set up to read bme280 sensor
+#
+import smbus2
+import bme280
+# Change bus and OD for the particular sensor config
+bme280_ID = 0x76
+bme280_bus = smbus2.SMBus(1)
+# Read the calibration and print to make sure it's working
+bme280_calibration = bme280.load_calibration_params(bme280_bus)
+print(bme280.sample(bme280_bus,bme280_ID,bme280_calibration))
+
+#
 # Set up rotating log file
+#
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import datetime
@@ -25,28 +42,48 @@ handler = TimedRotatingFileHandler(logfile,
                                    interval=1,
                                    backupCount=400)
 logger.addHandler(handler)
-
+#
 # Set up interrupt handler
+#
 if USING_GPIO:
     import RPi.GPIO as GPIO  
     GPIO.setmode(GPIO.BCM)  
 
-    interrupt_pin = 2
+    interrupt_pin = 22
     # GPIO set up as input. It is pulled up to stop false signals
-    GPIO.setup(interrupt_pin, GPIO.IN) # pin 2 has pull up already, pull_up_down=GPIO.PUD_UP)  
-
+    GPIO.setup(interrupt_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+#
+# Main loop waiting for interrupt
+#
 while True:
-    if USING_GPIO: GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)
-    interrupt_time = datetime.datetime.now()
-    with open(setfile) as f:
-        current_setting = f.readline().rstrip()
     try:
-        with open(temp_sensor) as f:
-            t=(f.read().split("t="))[1].rstrip()
-            t=(float(t)/1000)
-    except FileNotFoundError:
-        t=-99
-    logger.info(current_setting + " " + str(interrupt_time) + " " + str(t))
-    time.sleep(60) # anything within 1 minute is chiming the same hour. Don't wait too long as there are occasional false triggers
+        if USING_GPIO: GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)
+        interrupt_time = datetime.datetime.now()
+        
+        # Read the current Fine Adjust setting
+        with open(setfile) as f:
+            current_setting = f.readline().rstrip()
+            
+        # Read the current sensor data
+        bme280_data = bme280.sample(bme280_bus,bme280_ID,bme280_calibration)
+        
+        # Construct the information line for the log
+        info = current_setting + \
+          " " + str(interrupt_time) + \
+          " " + "{:.2f}".format(bme280_data.temperature) + \
+          " " + "{:.2f}".format(bme280_data.pressure) + \
+          " " + "{:.2f}".format(bme280_data.humidity)
+          
+        # Log the data
+        if USE_LOG:
+            logger.info(info)
+            time.sleep(60) # anything within 1 minute is chiming the same hour. Don't wait too long as there are occasional false triggers
+        else:
+            print(info)
+    except:
+        if USE_LOG:
+            logger.error("Exception detected, continuing:", sys.exc_info()[0])
+        else:
+            raise
     
 GPIO.cleanup()           # clean up GPIO on normal exit  
