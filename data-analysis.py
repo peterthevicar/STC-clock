3bfe68e08769d91f3a85852842f897465124ee5a
peterthevicar <peterbcs@gmail.com>
2021-05-21 00:43:21 +0100

    New calculation method: divide the data into two series: one for the tick chimes and one for the tock chimes (they are about 2 seconds apart). Use the longer series to calculate trend and error. Big difference is we're now trying to get the clock accurate to the mid-point between tick and tock so the clock is always about one second out, never 2. In the periods where the clock is always chiming on the tick, or always on the tock, the algorithm will make it accurate for that one series. If there is even one hour which chimes on the other tick/tock it will head for the mid-point.

--- data-analysis.py
+++ data-analysis.py
@@ -43,29 +43,94 @@ for inline in sys.stdin:
             report_first = False
         # ~ print("actual:", dt_actual, "nearest:", dt_nearest_min, "err:", err) # DEBUG
         cur_temp = float(s[3]) if len(s) >= 4 else -99
-        err_data.append([dt_secs, dt_nearest_min, err, fine_adjust, cur_temp, err-prev_err])
+        err_data.append([dt_secs, dt_nearest_min, err, fine_adjust, cur_temp])
         dt_nearest_min_prev = dt_nearest_min
         prev_err = err
 # ~ print(err_data) # DEBUG
-# Go through working out how far out of line each point is so we can identify outliers
-err_data[0][5] = -err_data[1][5]
-for i in range(1, len(err_data)-1):
-    err_data[i][5] = (err_data[i][5]-err_data[i+1][5])/2
-    
-# Calculate average, omitting the values most-different-from-neighbours-either-side as outliers
-outlier_percent=args.discard
-n_outliers = int(len(err_data)*outlier_percent/100)
-out_end = len(err_data)-n_outliers
+"""
+This part divides the data into two series, one for the chime-on-tick
+and the other for chime-on-tock.
+It does this by comparing the error for this datum with the previous point in the series,
+if there's a big change in the error then it assumes the datum belongs in the other series.
+Transients with a big error are discarded.
+"""
+def calc_d(err_d, curr_e):
+	if len(curr_e) > 0: # have previous data to compare with
+		return(abs(err_d[2]-curr_e[-1][2]))
+	else: # first point in this series so no difference yet
+		return 0
+series=[{'err_data':[],'avg_err':0}, {'err_data':[],'avg_err':0}]
+curr_s=0;
+for err_d in err_data:
+	# Compare this error with the previous one
+	d=calc_d(err_d, series[curr_s]['err_data'])
+	if d > 2.8: # something wrong, max should be about 2 seconds
+		# Spot the case where it's the first datum at issue (which would cause problems)
+		# and if so ignore BOTH data to be on the safe side
+		# ~ print("Too big",err_d[1])
+		if len(series[curr_s]['err_data']) == 1:
+			series[curr_s]['err_data'][:]=[]
+	else:
+		if d > 0.8: # difference too big, must be in the other series
+			# ~ print("Swap",err_d[1])
+			curr_s = 1-curr_s
+			d=calc_d(err_d, series[curr_s]['err_data'])
+		# Add to series
+		series[curr_s]['err_data'].append(err_d+[d])
+	
+"""
+This part goes through working out how far out of line each point is by
+calculating the average of d(prev) and d(next)
+"""
+for sn in 0,1:
+	tot_err=0
+	ed = series[sn]['err_data']
+	for i in range(0, len(ed)-1):
+		tot_err = tot_err + ed[i][2]
+		ed[i][5] = (ed[i][5] + ed[i+1][5]) / 2
+	series[sn]['avgerr'] = tot_err/len(ed) if len(ed)>0 else 0
+	# Calculate average, omitting the values most-different-from-neighbours-either-side as outliers
+	outlier_percent=args.discard
+	n_outliers = int(len(ed)*outlier_percent/100)
+	# ~ print("n_outliers", n_outliers)
+	out_end = len(ed)-n_outliers
+	series[sn]['sorted_ed']=sorted(ed, key=lambda ed: ed[5])[0:out_end]
+	# ~ print("Series", sn, "outliers:", n_outliers)
+	# ~ for s in series[sn]['sorted_ed']:
+		# ~ print(s[1],s[2],s[5])
+	# ~ print("Average error for whole series including outliers:", series[sn]['avgerr'])
+
+"""
+Now we have the two series, we have to choose the one with the most data
+to work out the trend, plus whether it's the fast or the slow series.
+We're aiming to get the clock centred between the two series so the error
+is never too bad (around 1s).
+If there's only one series then use that as it is. This will cause a bit 
+of 'hunting' when the clock is just switching between two and one series
+however this won't happen often and only calls for a 1s per day adjustment.
+
+adjust is added to the computed AVGERR before correcting the clock
+so if we're using the slow series (more positive) adjust needs to be -1,
+for the fast series (more negative) 1, and for a single series 0
+
+"""
+if len(series[1]['err_data'])==0: # single series
+	sn=0; adjust=0
+elif len(series[0]['err_data']) > len(series[1]['err_data']): # series 0 is longer
+	# use series 0 as it's longer
+	sn=0; adjust = 1 if (series[0]['avg_err'] < series[1]['avg_err']) else -1
+else: # series 1 is longer
+	sn=1; adjust = 1 if (series[1]['avg_err'] < series[0]['avg_err']) else -1
+# ~ print("Using series", sn, "with", len(series[sn]['err_data']), "data points and adjust set to", adjust)
+
+"""
+Now do the hard work of calculating the trend etc
+"""
+sorted_data = series[sn]['sorted_ed']
 sum_fa = 0
 sum_err = 0; n_err = 0; max_err = -100; min_err = 100
 sum_dt = 0
 sum_temp = 0; n_temp = 0
-sorted_data=sorted(err_data, key=lambda err_data: abs(err_data[5]))[0:out_end]
-# ~ print (n_outliers, "outliers:", sorted(err_data, key=lambda err_data: abs(err_data[5]))[out_end:]) # DEBUG
-
-# ~ med_min, med_err = sorted_data[len(sorted_data)//2][1:3]
-# ~ print("MEDER", "{:.3f}".format(med_err), "(Median Error at",med_min,", positive means clock is slow)")
-# ~ print (sorted(sorted_data)) # DEBUG
 if args.full: # full analysis requested
     for dt_secs, dt_nearest_min, err, fine_adjust, cur_temp, err_diff in sorted_data:
         sum_fa += fine_adjust
@@ -79,7 +144,7 @@ if args.full: # full analysis requested
         n_err += 1
         # ~ print(dt_secs, err)
     avg_err = sum_err / n_err
-    print("AVGER", "{:.3f}".format(avg_err), "(Average Error, positive means clock is slow)")
+    print("AVGER", "{:.3f}".format(avg_err+adjust), "(Average Error, positive means clock is slow.",abs(adjust), "subtracted" if adjust<0 else "added", "for tick/tock compensation)")
     # Now we know the average we can calculate the least-square slope
     avg_fa = sum_fa / n_err
     avg_dt_secs = sum_dt / n_err
@@ -102,4 +167,4 @@ if args.full: # full analysis requested
     print("  Maximum error:","{:.3f}".format(max_err),"= average error +","{:.3f}".format(max_err-avg_err))
     print("  Minimum error:","{:.3f}".format(min_err),"= average error -","{:.3f}".format(avg_err-min_err))
     print("  Spread of values (max-min):", "{:.3f}".format(max_err - min_err))
-    print("  Data points (including outliers): "+str(len(err_data))+", Outliers "+"("+str(outlier_percent)+"% most different from neighbours) discarded:",n_outliers)
+    print("  Data points in series (including outliers): "+str(len(series[sn]['err_data']))+", Outliers "+"("+str(outlier_percent)+"% most different from neighbours) discarded:",str(len(series[sn]['err_data'])-len(series[sn]['sorted_ed'])))
