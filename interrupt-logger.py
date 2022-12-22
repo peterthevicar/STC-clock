d0d8a7d5f6001a2d9cb8dfcf2f578d6f17ff3a48
peterthevicar <peter@lymingtonchurch.org>
2022-12-22 14:51:05 +0000

    Added USE_BME280 switch as we were getting errors from the serial bus

--- interrupt-logger.py
+++ interrupt-logger.py
@@ -3,6 +3,7 @@
 # For testing in different situations
 USING_GPIO=True
 USE_LOG=True
+USE_BME280=False
 
 # File paths
 datadir="/home/pi/Desktop/STC-clock/Data/"
@@ -13,11 +14,13 @@ setfile=datadir+"setting.txt"
 # Set up to read bme280 sensor
 #
 import smbus2
-import bme280
-# Change bus and ID for the particular sensor config
-bme280_bus = smbus2.SMBus(1)
-bme280_ID = 0x76
-bme280_calibration = bme280.load_calibration_params(bme280_bus)
+
+if USE_BME280:
+  import bme280
+  # Change bus and ID for the particular sensor config
+  bme280_bus = smbus2.SMBus(1)
+  bme280_ID = 0x76
+  bme280_calibration = bme280.load_calibration_params(bme280_bus)
 
 #
 # Set up rotating log file
@@ -55,20 +58,21 @@ while True:
         if USING_GPIO: GPIO.wait_for_edge(interrupt_pin, GPIO.FALLING)
         interrupt_time = datetime.datetime.now()
         
-        # Read the current Fine Adjust setting
+        # Get the current Fine Adjust setting and add the time of the interrupt
         with open(setfile) as f:
-            current_setting = f.readline().rstrip()
-            
-        # Read the current sensor data
-        bme280_data = bme280.sample(bme280_bus,bme280_ID,bme280_calibration)
-        
-        # Construct the information line for the log
-        info = current_setting + \
-          " " + str(interrupt_time) + \
-          " " + "{:.2f}".format(bme280_data.temperature) + \
-          " " + "{:.2f}".format(bme280_data.pressure) + \
-          " " + "{:.2f}".format(bme280_data.humidity)
-          
+            info = f.readline().rstrip() + \
+              " " + str(interrupt_time)
+
+        # Read the current sensor data if applicable
+        if USE_BME280:
+          bme280_data = bme280.sample(bme280_bus,bme280_ID,bme280_calibration)
+
+          # Construct the information line for the log
+          info = info + \
+            " " + "{:.2f}".format(bme280_data.temperature) + \
+            " " + "{:.2f}".format(bme280_data.pressure) + \
+            " " + "{:.2f}".format(bme280_data.humidity)
+
         # Log the data
         if USE_LOG:
             logger.info(info)
